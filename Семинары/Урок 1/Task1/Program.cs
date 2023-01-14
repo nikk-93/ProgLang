31eb8f4788afd909f498b12bad6ab503e1b95edb
NILadygin <nikk-93@mail.ru>
2023-01-14 17:23:14 +0500

    ksdf

--- Семинары/Урок 1/Task1/Program.cs
+++ Семинары/Урок 1/Task1/Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.Write("Введите число: ");
-string? text = Console.ReadLine();
-// int number = Convert.ToInt32(Console.ReadLine());
-int number = text == null ? 0 : int.Parse(text);
+// string? text = Console.ReadLine();
+// // int number = Convert.ToInt32(Console.ReadLine());
+// int number = text == null ? 0 : int.Parse(text);
+int number = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("Квадрат числа: {0}", Math.Pow(number, 2));
---
+++ Семинары/Урок 1/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите первое число: ");
+int firstNum = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int secondNum = int.Parse(Console.ReadLine() ?? "0");
+
+// 1-ый способ
+Console.WriteLine("Максимальное число: {0}", Math.Max(firstNum, secondNum));
+
+Console.WriteLine("Минимальное число: {0}", Math.Min(firstNum, secondNum));
+
+// 2-ой способ
+if (firstNum > secondNum)
+{
+    Console.WriteLine("Максимальное число: {0}", firstNum);
+    Console.WriteLine("Минимальное число: {0}", secondNum);
+}
+else
+{
+    Console.WriteLine("Максимальное число: {0}", secondNum);
+    Console.WriteLine("Минимальное число: {0}", firstNum);
+}
