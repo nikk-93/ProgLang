612ed7be857125df34d2d4a2c6493284dfac328f
NILadygin <nikk-93@mail.ru>
2023-01-14 16:56:45 +0500

    sdf

--- Семинары/Урок 1/Task1/Program.cs
+++ Семинары/Урок 1/Task1/Program.cs
@@ -1,4 +1,5 @@
-﻿string? text = Console.ReadLine();
+﻿Console.Write("Введите число: ");
+string? text = Console.ReadLine();
 // int number = Convert.ToInt32(Console.ReadLine());
 int number = text == null ? 0 : int.Parse(text);
 Console.WriteLine("Квадрат числа: {0}", Math.Pow(number, 2));
