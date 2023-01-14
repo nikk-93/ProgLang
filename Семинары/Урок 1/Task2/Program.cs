cab0da3ba7e6cd6fa709e1dd9ba8a4062d183ce3
NILadygin <nikk-93@mail.ru>
2023-01-14 17:52:28 +0500

    sljdf

--- Семинары/Урок 1/Task2/Program.cs
+++ Семинары/Урок 1/Task2/Program.cs
@@ -4,18 +4,20 @@ Console.Write("Введите второе число: ");
 int secondNum = int.Parse(Console.ReadLine() ?? "0");
 
 // 1-ый способ
-Console.WriteLine("Максимальное число: {0}", Math.Max(firstNum, secondNum));
+var max = Math.Max(firstNum, secondNum);
+var min = Math.Min(firstNum, secondNum);
 
-Console.WriteLine("Минимальное число: {0}", Math.Min(firstNum, secondNum));
+// // 2-ой способ
+// if (firstNum > secondNum)
+// {
+//     max = firstNum;
+//     min = secondNum;
+// }
+// else
+// {
+//     max = secondNum;
+//     min = firstNum;
+// }
 
-// 2-ой способ
-if (firstNum > secondNum)
-{
-    Console.WriteLine("Максимальное число: {0}", firstNum);
-    Console.WriteLine("Минимальное число: {0}", secondNum);
-}
-else
-{
-    Console.WriteLine("Максимальное число: {0}", secondNum);
-    Console.WriteLine("Минимальное число: {0}", firstNum);
-}
+Console.WriteLine("Максимальное число: {0}", max);
+Console.WriteLine("Минимальное число: {0}", min);
---
+++ Семинары/Урок 1/Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+for (int i = -number; i < number; i++)
+{
+    Console.WriteLine(i);
+}
