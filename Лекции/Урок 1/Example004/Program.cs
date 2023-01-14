aa43aa13221aa5a2243dee029585cd692d654cb7
NILadygin <nikk-93@mail.ru>
2023-01-14 16:22:40 +0500

    asd

---
+++ Лекции/Урок 1/Example004/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("Hello, World!");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+string symbol = "*";
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine(symbol);
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine(symbol);
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine(symbol);
+
+
+int x = xa, y = yb;
+
+int count = 0;
+
+while (count < 1000000)
+{
+    int what = new Random().Next(0, 3);
+
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine(symbol);
+    count++;
+}
