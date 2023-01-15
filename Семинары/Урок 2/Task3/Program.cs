338986885696d27439a9c8f37ff08ddc579b979b
NILadygin <nikk-93@mail.ru>
2023-01-15 17:38:07 +0500

    asdfxaz

---
+++ Семинары/Урок 2/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿List<int> digits = new List<int>();
+int modnum;
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int num = number;
+
+do
+{
+    modnum = num % 10;
+    digits.Add(modnum);
+    num /= 10;
+} while (num > 0);
+
+bool isPol = true;
+for (int i = 0; i < digits.Count / 2; i++)
+{
+    if (digits[i] != digits[(digits.Count - 1) - i])
+        isPol = false;
+}
+
+if (isPol)
+    Console.WriteLine("Число {0} является полиндромом", number);
+else
+    Console.WriteLine("Число {0} не является полиндромом", number);
